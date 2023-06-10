e8c90398510d850b1ebb615e2e2958898d98f782
blck-snwmn <whitesnowmancreator@gmail.com>
2023-06-11 03:23:39 +0900

    Send screenshot to slack

--- src/worker.ts
+++ src/worker.ts
@@ -3,11 +3,11 @@ import puppeteer from "@cloudflare/puppeteer";
 export interface Env {
 	TARGET_URL: string
 	WEBHOOK_URL: string
-	ACCESS_TOKEN: string
+	SLACK_TOKEN: string
 	BROWSER: puppeteer.BrowserWorker
 }
 
-async function name(env: Env) {
+async function send(env: Env): Promise<Response> {
 	const browser = await puppeteer.launch(env.BROWSER);
 	const page = await browser.newPage();
 	await page.setViewport({ width: 800, height: 2000 });
@@ -16,6 +16,21 @@ async function name(env: Env) {
 
 	const img = (await page.screenshot()) as Buffer;
 	await browser.close();
+
+	const blob = new Blob([img.buffer], { type: "image/png" })
+
+	const fd = new FormData()
+	fd.append("initial_comment", "schedule")
+	fd.append("channels", "C05CACLMRUZ")
+	fd.append("file", blob)
+
+	return fetch("https://slack.com/api/files.upload", {
+		method: "POST",
+		headers: {
+			"Authorization": `Bearer ${env.SLACK_TOKEN}`
+		},
+		body: fd,
+	})
 }
 
 export default {
