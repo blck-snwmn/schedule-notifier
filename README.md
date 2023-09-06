9e64f270a2dd58dc669a8049a9d153967d735d8f
blck-snwmn <whitesnowmancreator@gmail.com>
2023-09-07 01:44:49 +0900

    Format by deno

--- README.md
+++ README.md
@@ -1,32 +1,40 @@
 # schedule-notifier
+
 [![CodeQL](https://github.com/blck-snwmn/schedule-notifier/actions/workflows/github-code-scanning/codeql/badge.svg)](https://github.com/blck-snwmn/schedule-notifier/actions/workflows/github-code-scanning/codeql)
 
-A Cloudflare Worker that navigates to a target URL, captures a screenshot of the page content, and posts the screenshot to Slack.
+A Cloudflare Worker that navigates to a target URL, captures a screenshot of the
+page content, and posts the screenshot to Slack.
 
 ## Setting(for Local)
+
 Please follow these steps to set up the project:
 
 1. Create a file named .dev.vars in the project root directory.
 2. Write the following key-value pairs in the .dev.vars file:
-  ```toml
-  TARGET_URL=<Your_Target_URL>
-  SLACK_TOKEN=<Your_Slack_Bearer_Token>
-  ```
 
-Replace 
+```toml
+TARGET_URL=<Your_Target_URL>
+SLACK_TOKEN=<Your_Slack_Bearer_Token>
+```
+
+Replace
+
 - <Your_Target_URL> with the URL you want to capture a screenshot of
 - <Your_Slack_Bearer_Token> with the bearer token for the Slack file upload API
 
 ## Setting
+
 Run the following commands to add your secrets to the Workers configuration:
+
 ```bash
 wrangler secret put TARGET_URL
 wrangler secret put SLACK_TOKEN
 ```
 
 ## Deploy
+
 After you've added the secrets, deploy the Worker with the following command:
+
 ```bash
 wrangler deploy
 ```
-
--- src/worker.ts
+++ src/worker.ts
@@ -2,85 +2,87 @@ import puppeteer from "@cloudflare/puppeteer";
 import robotsParser from "robots-parser";
 
 export interface Env {
-	TARGET_URL: string
-	SLACK_TOKEN: string
-	BROWSER: puppeteer.BrowserWorker
+  TARGET_URL: string;
+  SLACK_TOKEN: string;
+  BROWSER: puppeteer.BrowserWorker;
 }
 
 async function send(env: Env): Promise<Response> {
-	let isAllowed = true;
-	try {
-		const robotsTextPath = new URL(env.TARGET_URL).origin + "/robots.txt";
-		const response = await fetch(robotsTextPath);
+  let isAllowed = true;
+  try {
+    const robotsTextPath = new URL(env.TARGET_URL).origin + "/robots.txt";
+    const response = await fetch(robotsTextPath);
 
-		const robots = robotsParser(robotsTextPath, await response.text());
-		isAllowed = robots.isAllowed(env.TARGET_URL) ?? true; // respect robots.txt!
-	} catch { }
+    const robots = robotsParser(robotsTextPath, await response.text());
+    isAllowed = robots.isAllowed(env.TARGET_URL) ?? true; // respect robots.txt!
+  } catch {
+    // ignore
+  }
 
-	if (!isAllowed) {
-		console.log("robots.txt disallows crawling");
-		return new Response("robots.txt disallows crawling", { status: 403 });
-	}
-	console.log("robots.txt allows crawling");
+  if (!isAllowed) {
+    console.log("robots.txt disallows crawling");
+    return new Response("robots.txt disallows crawling", { status: 403 });
+  }
+  console.log("robots.txt allows crawling");
 
-	const browser = await puppeteer.launch(env.BROWSER);
-	const page = await browser.newPage();
-	await page.setViewport({ width: 800, height: 2000 });
+  const browser = await puppeteer.launch(env.BROWSER);
+  const page = await browser.newPage();
+  await page.setViewport({ width: 800, height: 2000 });
 
-	await page.goto(env.TARGET_URL);
+  await page.goto(env.TARGET_URL);
 
-	const img = (await page.screenshot()) as Buffer;
-	await browser.close();
+  const img = (await page.screenshot()) as Buffer;
+  await browser.close();
 
-	const blob = new Blob([img.buffer], { type: "image/png" })
+  const blob = new Blob([img.buffer], { type: "image/png" });
 
-	const fd = new FormData()
-	fd.append("initial_comment", "schedule")
-	fd.append("channels", "C05CACLMRUZ")
-	fd.append("file", blob)
+  const fd = new FormData();
+  fd.append("initial_comment", "schedule");
+  fd.append("channels", "C05CACLMRUZ");
+  fd.append("file", blob);
 
-	return fetch("https://slack.com/api/files.upload", {
-		method: "POST",
-		headers: {
-			"Authorization": `Bearer ${env.SLACK_TOKEN}`
-		},
-		body: fd,
-	})
+  return fetch("https://slack.com/api/files.upload", {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${env.SLACK_TOKEN}`,
+    },
+    body: fd,
+  });
 }
 
 export default {
-	// for test
-	// async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-	// 	const resp = await send(env);
-	// 	console.log(`status: ${resp.status}`)
-	// 	if (!resp.ok) {
-	// 		console.log(`result: ${await resp.text()}`)
-	// 		return new Response("failed", { status: resp.status })
-	// 	}
-	// 	const body = await resp.text()
-	// 	const json = JSON.parse(body) as { ok: boolean }
-	// 	if (!json["ok"]) {
-	// 		console.log(`result: ${body}`)
-	// 		return new Response("failed", { status: resp.status })
-	// 	}
-	// 	return new Response("ok")
-	// },
-	async scheduled(
-		controller: ScheduledController,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<void> {
-		const resp = await send(env);
-		console.log(`status: ${resp.status}`)
-		if (!resp.ok) {
-			console.log(`result: ${await resp.text()}`)
-			return
-		}
-		const body = await resp.text()
-		const json = JSON.parse(body) as { ok: boolean }
-		if (!json["ok"]) {
-			console.log(`result: ${body}`)
-			return
-		}
-	}
+  // for test
+  // async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+  // 	const resp = await send(env);
+  // 	console.log(`status: ${resp.status}`)
+  // 	if (!resp.ok) {
+  // 		console.log(`result: ${await resp.text()}`)
+  // 		return new Response("failed", { status: resp.status })
+  // 	}
+  // 	const body = await resp.text()
+  // 	const json = JSON.parse(body) as { ok: boolean }
+  // 	if (!json["ok"]) {
+  // 		console.log(`result: ${body}`)
+  // 		return new Response("failed", { status: resp.status })
+  // 	}
+  // 	return new Response("ok")
+  // },
+  async scheduled(
+    _controller: ScheduledController,
+    env: Env,
+    _ctx: ExecutionContext,
+  ): Promise<void> {
+    const resp = await send(env);
+    console.log(`status: ${resp.status}`);
+    if (!resp.ok) {
+      console.log(`result: ${await resp.text()}`);
+      return;
+    }
+    const body = await resp.text();
+    const json = JSON.parse(body) as { ok: boolean };
+    if (!json["ok"]) {
+      console.log(`result: ${body}`);
+      return;
+    }
+  },
 };
